788bc59ba688621b87fb85e61245222466ccf226
Logan <logan@mac.home>
2026-05-25 19:14:52 +0200

    Fix recaptcha initializer for production

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,10 @@
 Recaptcha.configure do |config|
-  config.site_key   = Rails.application.credentials.dig(:recaptcha_site_key)
-  config.secret_key = Rails.application.credentials.dig(:recaptcha_secret_key)
+  # On essaie de lire les credentials, si ça plante ou si c'est vide, on met nil au lieu de crasher
+  begin
+    config.site_key = Rails.application.credentials.dig(:recaptcha, :site_key) || ENV['RECAPTCHA_SITE_KEY']
+    config.secret_key = Rails.application.credentials.dig(:recaptcha, :secret_key) || ENV['RECAPTCHA_SECRET_KEY']
+  rescue StandardError
+    config.site_key = ENV['RECAPTCHA_SITE_KEY']
+    config.secret_key = ENV['RECAPTCHA_SECRET_KEY']
+  end
 end
