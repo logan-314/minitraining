ee3174dcf50f909076225ed9d5d21ebaef43e69f
Logan <logan@mac.home>
2026-05-25 19:29:22 +0200

    Désactivation du crash sur credentials en production

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -9,13 +9,19 @@
 #  :enable_starttls_auto => true
 #}
 
-# Settings to send with noreply@mathraining.be
-ActionMailer::Base.smtp_settings = {
-  :address              => "pro3.mail.ovh.net",
-  :port                 => 587,
-  :domain               => "mathraining.be",
-  :user_name            => Rails.application.credentials.dig(:email_user_name),
-  :password             => Rails.application.credentials.dig(:email_password),
-  :authentication       => "login",
-  :enable_starttls_auto => true
-}
+begin
+  # Settings to send with noreply@mathraining.be
+  ActionMailer::Base.smtp_settings = {
+    :address              => "pro3.mail.ovh.net",
+    :port                 => 587,
+    :domain               => "mathraining.be",
+    :user_name            => Rails.application.credentials.dig(:email_user_name),
+    :password             => Rails.application.credentials.dig(:email_password),
+    :authentication       => "login",
+    :enable_starttls_auto => true
+  }
+rescue ActiveSupport::MessageEncryptor::InvalidMessage, NameError => e
+  # Si la clé ne déchiffre pas les credentials, Rails ignore poliment l'erreur
+  # et n'empêche pas le serveur de démarrer !
+  Rails.logger.warn "Attention: Les credentials SMTP n'ont pas pu être déchiffrés (#{e.message})"
+end
